0711a753fa4036b27d628312c6b3b366ab96284e
Mikhaylov.V <mihailov.v@fhclub.ru>
2021-03-29 12:44:18 +0300

    added Confirmation Order Event

---
+++ src/Events/ConfirmationOrderEvent.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Vladmeh\PaymentManager\Events;
+
+
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Vladmeh\PaymentManager\Models\Order;
+
+class ConfirmationOrderEvent
+{
+    use Dispatchable, SerializesModels;
+
+    /**
+     * @var Order
+     */
+    public $order;
+    /**
+     * @var array
+     */
+    public $paymentData;
+
+    /**
+     * ConfirmationOrderEvent constructor.
+     * @param $order
+     * @param array $paymentData
+     */
+    public function __construct(Order $order, array $paymentData)
+    {
+        $this->order = $order;
+        $this->paymentData = $paymentData;
+    }
+}
