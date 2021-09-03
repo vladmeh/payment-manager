c84b6d453229582f2f6db805d8e9ee6a6d320d95
mihailov.v <mihailov.v@fhclub.ru>
2021-09-03 22:04:19 +0300

    Remove NotificationRequest

--- src/Pscb/PscbServiceProvider.php
+++ src/Pscb/PscbServiceProvider.php
@@ -9,20 +9,12 @@ class PscbServiceProvider extends ServiceProvider
     public function register()
     {
         $this->registerPscbService();
-        $this->resolvingRequests();
     }
 
     private function registerPscbService(): void
     {
         $this->app->bind('payment.system.pscb', function () {
             return new PaymentService();
-        });
-    }
-
-    private function resolvingRequests(): void
-    {
-        $this->app->resolving(NotificationRequest::class, function ($request, $app) {
-            NotificationRequest::createFrom($app['request'], $request);
         });
     }
 }
